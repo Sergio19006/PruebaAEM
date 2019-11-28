1a139519d6f9948d89649097ae783f5f88677cdd
Sergio <sergiodphn@gmail.com>
2019-11-28 11:08:43 +0000

    config to parsys added

--- core/src/main/java/com/adobe/aem/guides/wknd/core/models/BreadCumb.java
+++ core/src/main/java/com/adobe/aem/guides/wknd/core/models/BreadCumb.java
@@ -1,50 +1,44 @@
 package com.adobe.aem.guides.wknd.core.models;
 
-import com.adobe.aem.guides.wknd.core.types.Article;
+import com.adobe.aem.guides.wknd.core.types.Nav;
 import com.day.cq.wcm.api.Page;
 import com.day.cq.wcm.api.PageManager;
 import org.apache.sling.api.resource.Resource;
 import org.apache.sling.api.resource.ResourceResolver;
 import org.apache.sling.models.annotations.Model;
+import org.apache.sling.models.annotations.Optional;
 import org.apache.sling.models.annotations.injectorspecific.Self;
 
 import javax.annotation.PostConstruct;
 import javax.inject.Inject;
 import java.util.ArrayList;
-import java.util.Iterator;
+import java.util.Arrays;
 import java.util.List;
 
 @Model(adaptables = Resource.class)
-public class ListArticle {
+public class BreadCumb {
 
-    private List<Article> articleList;
-
+    @Inject @Optional
+    private Page currentPage;
     @Inject
     ResourceResolver resourceResolver;
-
     @Self
     private Resource resource;
+    private List<Nav> paths;
 
     @PostConstruct
-    public void init() {
-
-        articleList = new ArrayList<Article>();
-
+    public void init(){
+        paths = new ArrayList<Nav>();
         PageManager pageManager = resourceResolver.adaptTo(PageManager.class);
-
         Page currentPage = pageManager.getContainingPage(resource);
-        Iterator<Page> iterator = currentPage.listChildren();
-        try {
-            while (iterator.hasNext()) {
-                Page page = iterator.next();
-                articleList.add(new Article(page.getTitle(),page.getPath()));
-            }
-        }catch (Exception e){
-            System.out.println(e);
+
+        while((currentPage.getDepth() > 2)){
+            paths.add(0, new Nav(currentPage.getTitle(),currentPage.getPath()));
+            currentPage = currentPage.getParent();
         }
     }
 
-    public List<Article> getArticleList() {
-        return articleList;
+    public List<Nav> getPath() {
+        return paths;
     }
 }
